02aabfe3832dec32abd2035d4a0cb6c6797fa3a8
Salty-fs <1362516752@qq.com>
2021-11-26 23:00:22 +0800

    0.0.6

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.get('/',(req,res) =>{
 app.post('/getforce',(req,res) =>{
   let data = JSON.stringify(req.body) +"\r\n"
   console.log("data",req.data);
-  console.log("body",eq.body);
+  console.log("body",req.body);
   //触发事件   
   try{
     myevent.emit('abc',data)
